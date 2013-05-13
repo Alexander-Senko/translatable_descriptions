c5dc696b6c4ad13b2a757c8a9ca4618ae207f2b5
Alexander Senko <Alexander.Senko@gmail.com>
2013-05-14 06:25:27 +0700

    API: added Describable mixin to use in models.

--- lib/translatable_descriptions.rb
+++ lib/translatable_descriptions.rb
@@ -1,4 +1,12 @@
 require 'translatable_descriptions/engine'
 
 module TranslatableDescriptions
+	autoload :Describable, 'translatable_descriptions/describable'
+
+	mattr_accessor :editable_attributes
+	self.editable_attributes = [
+		:title,
+		:abstract,
+		:body,
+	]
 end
---
+++ lib/translatable_descriptions/describable.rb
@@ -0,0 +1,46 @@
+require 'active_support/concern'
+
+module TranslatableDescriptions::Describable
+	extend ActiveSupport::Concern
+
+	included do
+		scope :translated, -> (lang = I18n.locale) {
+			includes(:descriptions).
+				where descriptions: { id: Description.in_language(lang) } # TODO: simplify
+		}
+
+		delegate *TranslatableDescriptions.editable_attributes, to: :description
+
+		for lang in I18n.available_locales do
+			-> (lang) {
+				has_one :"description_#{lang}_relation", -> { where(
+					target_type: Description,
+					target_id:   Description.in_language(lang)
+				)}, reflections[
+					reflections[:descriptions].options[:through]
+				].options
+
+				has_one :"description_#{lang}", reflections[:descriptions].options.merge({
+					through:   :"description_#{lang}_relation",
+					dependent: :destroy,
+				})
+
+				accepts_nested_attributes_for :"description_#{lang}",
+					update_only: true, reject_if: -> (attributes) {
+						attributes.values_at(
+							*TranslatableDescriptions.editable_attributes
+						).reject(&:blank?).blank?
+					}
+
+				define_method "build_description_#{lang}" do |attributes = {}|
+					descriptions.build attributes.merge(lang: lang)
+				end
+			}.(lang)
+		end
+	end
+
+	def description lang = I18n.locale
+		send "description_#{lang}" or
+			send "build_description_#{lang}"
+	end
+end
