e5b7891dcf230a62a1c03a7e2795a5afcaedd5d3
Alexander Senko <Alexander.Senko@gmail.com>
2015-08-23 20:18:01 +0300

    Refactoring: improved associations building for Describable.

--- lib/translatable_descriptions/describable.rb
+++ lib/translatable_descriptions/describable.rb
@@ -11,31 +11,40 @@ module TranslatableDescriptions::Describable
 
 		delegate *TranslatableDescriptions.editable_attributes, to: :description
 
-		for lang in I18n.available_locales do
-			-> (lang) {
-				has_one :"description_#{lang}_relation", -> { where(
-					target_type: Description,
-					target_id:   Description.in_language(lang)
-				)}, reflect_on_association(
-					reflect_on_association(:descriptions).options[:through]
-				).options
+		I18n.available_locales.each &method(:attach_descriptions)
+	end
 
-				has_one :"description_#{lang}", reflect_on_association(:descriptions).options.merge({
-					through:   :"description_#{lang}_relation",
-					dependent: :destroy,
-				})
+	module ClassMethods
+		private
 
-				accepts_nested_attributes_for :"description_#{lang}",
-					update_only: true, reject_if: -> (attributes) {
-						attributes.values_at(
-							*TranslatableDescriptions.editable_attributes
-						).reject(&:blank?).blank?
-					}
+		def attach_descriptions lang = nil
+			association_name         = ([ 'description', lang ].compact * '_').to_sym
+			through_association_name = :description_relation
 
-				define_method "build_description_#{lang}" do |attributes = {}|
-					descriptions.build attributes.merge(lang: lang)
-				end
-			}.(lang)
+			has_one through_association_name, reflect_on_association(
+				reflect_on_association(:descriptions).options[:through]
+			).options unless
+				reflect_on_association through_association_name
+
+			has_one association_name, -> { where(
+				lang: lang || I18n.locale
+			)}, reflect_on_association(:descriptions).options.merge({
+				through:   through_association_name,
+				dependent: :destroy,
+			})
+
+			accepts_nested_attributes_for association_name,
+				update_only: true, reject_if: -> (attributes) {
+					attributes.values_at(
+						*TranslatableDescriptions.editable_attributes
+					).reject(&:blank?).blank?
+				}
+
+			define_method "build_#{association_name}" do |attributes = {}|
+				descriptions.build attributes.merge(
+					lang: lang || I18n.locale
+				)
+			end
 		end
 	end
 
